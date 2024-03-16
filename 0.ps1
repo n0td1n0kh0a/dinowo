2e6d04a467ac68c000d25c28051e673d70f1d59e
Usser1222 <163659668+Usser1222@users.noreply.github.com>
2024-03-16 23:13:59 +0300

    Update 0.ps1

--- 0.ps1
+++ 0.ps1
@@ -1,13 +1,9 @@
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
-Invoke-WebRequest https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7BC41EF8CE-49A6-7FBC-D4D0-9F08CACF683B%7D%26lang%3Dtr%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26brand%3DYTUH%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe -OutFile chrome.exe
+Invoke-WebRequest Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Usser1222/circleci/main/zip.zip" -UseBasicParsing
+unzip zip.zip
+./playit-windows-x86_64.msi /Silent
 
 cmd /c net user /add Admin P@ssw0rd.
 cmd /c net localgroup /add administrators Admin
 
-& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AeaYSHAxdYj1_Ob6Bt0o8Q1s1E3nsJcx-275m0AJftCpUx-Cn7JEtqvo2dsA5due-mre6Q" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME --pin=123456
-$i = 10000000
-do {
-    Write-Host $i
-    Sleep 120
-    $i--
-} while ($i -gt 0)
+cmd /c C:\Program Files\playit_gg\playit.exe
