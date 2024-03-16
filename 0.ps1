6ba4875731ede7129a46e2f95544c889976f1b12
Usser1222 <163659668+Usser1222@users.noreply.github.com>
2024-03-17 00:57:40 +0300

    Update 0.ps1

--- 0.ps1
+++ 0.ps1
@@ -1,7 +1,7 @@
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
 
 curl -o zip.zip -Uri "https://raw.githubusercontent.com/Usser1222/circleci/main/zip.zip"
-& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AeaYSHAHtBLtqHR1lzEwgj_zAV4Q8c6quil0KIMELRVrqzbsvXBykCDk5x74xd-KjtnwBg" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME --pin=123456
+& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AeaYSHAs48VfgbVbKI0xl3Yk1jtTj72CYYvYDYNFAuBvVyOIACb6J1nWQntZEpQqRdQ70A" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME --pin=123456
 
 unzip zip.zip
 ./playit-windows-x86_64-signed.msi /Silent
