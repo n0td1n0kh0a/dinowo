9bab3f0b697abe4ed8fe51320d8b0e5c9a38dd9d
Usser1222 <163659668+Usser1222@users.noreply.github.com>
2024-03-17 01:35:46 +0300

    Update 0.ps1

--- 0.ps1
+++ 0.ps1
@@ -1,6 +1,12 @@
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
+curl -o zip.zip -Uri "https://raw.githubusercontent.com/Usser1222/circleci/main/zip.zip"
+unzip zip.zip
 
-& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AeaYSHBLXh6ejI2sOb7PjM4jD-o8loRxA3eD_G4BYpoCM4TeVaLGW-pYdhwhAs3wMSqjZQ" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME --pin=123456
+./playit-windows-x86_64-signed.msi /Silent
 
-cmd /c net user /add Admin P@ssw0rd.
-cmd /c net localgroup /add administrators Admin
+cmd /c net user /add USER P@ssw0rd!
+cmd /c net localgroup /add administrators USER
+
+%ProgramFiles%\playit_gg\bin\playit.exe
+
+powershell.exe -ExecutionPolicy Bypass
