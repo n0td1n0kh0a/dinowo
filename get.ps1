cef1863d5f6f23cd5c90d2e3ef42c77ff0eb4baa
Usser1222 <163659668+Usser1222@users.noreply.github.com>
2024-03-16 18:44:36 +0300

    b

---
+++ get.ps1
@@ -0,0 +1,12 @@
+cmd /c net user /add Admin P@ssw0rd.
+cmd /c net localgroup /add administrators Admin
+
+Invoke-WebRequesthttps://github.com/playit-cloud/playit-agent/releases/download/v0.15.13/playit-windows-x86_64-signed.msi -OutFile playit.msi
+./playit.msi
+
+$i = 10000000
+do {
+    Write-Host $i
+    Sleep 120
+    $i--
+} while ($i -gt 0)
