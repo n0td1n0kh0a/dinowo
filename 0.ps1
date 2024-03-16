084849e4d49859297addbab518b722f5f1d39bbe
Usser1222 <163659668+Usser1222@users.noreply.github.com>
2024-03-16 23:18:44 +0300

    Update 0.ps1

--- 0.ps1
+++ 0.ps1
@@ -1,5 +1,5 @@
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
-Invoke-WebRequest Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Usser1222/circleci/main/zip.zip" -UseBasicParsing
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Usser1222/circleci/main/zip.zip" -UseBasicParsing
 unzip zip.zip
 ./playit-windows-x86_64.msi /Silent
 
