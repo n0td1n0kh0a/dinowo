23f386e91d1d9b9400500559d6b58189319b8129
n0td1n0kh0a <152876934+n0td1n0kh0a@users.noreply.github.com>
2024-03-18 21:58:48 +0700

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,7 +1,2 @@
-$username = 'FreeRDP_Community'
-$password = 'Din0_On_T0p!'
-$credential = New-Object System.Management.Automation.PSCredential ($username, $securePassword)
 
-Invoke-Command -ComputerName localhost -Credential $credential -ScriptBlock {
-& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AeaYSHCzRMmw-Y7aqSbesilDBQK_Z6CZtEYDaEh8Uqq9KW5z1EB6xxQWWRx_KMKjaDlyzQ" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME --pin=696969
-}
+& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AeaYSHA4mUcIleqFaPBWArpg-D9xrS_qNbraW4zbIZFtw17cJsa3d_MhsD-T1CDcT4Fz9g" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME --pin=696969
