570e73fa392f3fc8d291e1c7c312db55b2df469b
n0td1n0kh0a <152876934+n0td1n0kh0a@users.noreply.github.com>
2024-03-23 08:22:23 +0700

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,6 +1,2 @@
-$username = 'FreeRDP_Community'
-$password = ''
-$securePassword = ConvertTo-SecureString $password -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential ($username, $securePassword)
-& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AeaYSHDxCHr2Sl7sbVnPDM64JwzniPdpjNocZGlY-GtLOBdiPrPvpZ89neSGkfNDcgxSzw" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME
-}
+
+& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AeaYSHDxCHr2Sl7sbVnPDM64JwzniPdpjNocZGlY-GtLOBdiPrPvpZ89neSGkfNDcgxSzw" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME --pin=696969
