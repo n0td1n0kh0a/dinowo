e3d8f30c977f4a2eda407d0dc51b9b96107e9f70
n0td1n0kh0a <152876934+n0td1n0kh0a@users.noreply.github.com>
2024-04-01 18:40:36 +0700

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,1 +1,1 @@
-& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AeaYSHDY3PW_s92UHGOXJtdeN5Jk9JXd82mZlckudPv7wP1rg8v256DMeAtxBfTltZtSrA" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME --pin=000000
+& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AeaYSHBiaDhprPkGn_eYG722mXlyTTAGlmGaliXi3evByWEd34d7wLUJMv7VnMxsfmtJsg" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME --pin=696969
